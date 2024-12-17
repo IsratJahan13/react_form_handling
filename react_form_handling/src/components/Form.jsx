b11ee3d0faeb47a85577358a9620bb2836066611
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-12-17 11:42:01 +0200

    use register and handleSubmit functions from useForm hook

--- react_form_handling/src/components/Form.jsx
+++ react_form_handling/src/components/Form.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Form = () => {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Name:</label>
-        <input type="text" placeholder="Full name..." />
+        <input
+          type="text"
+          placeholder="Full name..."
+          {...register('fullName')}
+        />
       </div>
       <div>
         <label>Email:</label>
-        <input type="text" placeholder="Email..." />
+        <input type="text" placeholder="Email..." {...register('email')} />
       </div>
       <div>
         <label>Age:</label>
-        <input type="text" placeholder="Age..." />
+        <input type="number" placeholder="Age..." {...register('age')} />
       </div>
       <div>
         <label>Password:</label>
-        <input type="password" placeholder="Password..." />
+        <input
+          type="password"
+          placeholder="Password..."
+          {...register('password')}
+        />
       </div>
       <div>
         <label>Confirm password:</label>
-        <input type="password" placeholder="Confirm Password..." />
+        <input
+          type="password"
+          placeholder="Confirm Password..."
+          {...register('confirmPassword')}
+        />
       </div>
       <input type="submit" />
     </form>
